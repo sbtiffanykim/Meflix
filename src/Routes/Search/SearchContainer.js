1b457d5e514bbbde60a138db2c74c5662385cb19
Tiffnay Kim <sbtiffanykim@gmail.com>
2021-06-21 22:22:15 +0900

    Done with the search container

---
+++ src/Routes/Search/SearchContainer.js
@@ -0,0 +1,53 @@
+import { moviesApi, tvApi } from "api";
+import React from "react";
+import SearchPresenter from "./SearchPresenter";
+
+// eslint-disable-next-line
+export default class extends React.Component {
+  state = {
+    movieResults: null,
+    tvResults: null,
+    searchTerm: "",
+    loading: false,
+    error: null,
+  };
+
+  handleSubmit = () => {
+    const { searchTerm } = this.state;
+    if (searchTerm !== "") {
+      this.searchByTerm();
+    }
+  };
+
+  searchByTerm = async () => {
+    const { searchTerm } = this.state;
+    this.setState({ loading: true });
+    try {
+      const {
+        data: { results: movieResults },
+      } = await moviesApi.movieSearch(searchTerm);
+      const {
+        data: { results: tvResults },
+      } = await tvApi.tvShowSearch(searchTerm);
+      this.setState({ movieResults, tvResults });
+    } catch {
+      this.setState({ error: "Cannot find any corresponding information" });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
+  render() {
+    const { movieResults, tvResults, searchTerm, loading, error } = this.state;
+    return (
+      <SearchPresenter
+        movieResults={movieResults}
+        tvResults={tvResults}
+        searchTerm={searchTerm}
+        loading={loading}
+        error={error}
+        handleSubmit={this.handleSubmit}
+      />
+    );
+  }
+}
