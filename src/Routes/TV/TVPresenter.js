429e4b0c319d14ff3acaac62225b8bbee048ad3b
Tiffnay Kim <sbtiffanykim@gmail.com>
2021-06-22 00:04:25 +0900

    render the finding results in tv & movie presenters

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,9 +1,34 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Section from "Components/Section";
+
+const Container = styled.div`
+  padding: 0 10px;
+`;
 
 const TVPresenter = ({ topRated, popular, airingToday, loading, error }) =>
-  null;
+  loading ? null : (
+    <Container>
+      {topRated && topRated.length > 0 && (
+        <Section title="Top Rated Show">
+          {topRated.map((show) => show.name)}
+        </Section>
+      )}
+
+      {popular && popular.length > 0 && (
+        <Section title="Popular Show">
+          {popular.map((show) => show.name)}
+        </Section>
+      )}
+
+      {airingToday && airingToday.length > 0 && (
+        <Section title="Airing Today">
+          {airingToday.map((show) => show.name)}
+        </Section>
+      )}
+    </Container>
+  );
 
 TVPresenter.propTypes = {
   topRated: PropTypes.array,
