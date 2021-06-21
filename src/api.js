337220fa88ed12c7de5209980bb1c5b8914c999f
Tiffnay Kim <sbtiffanykim@gmail.com>
2021-06-21 11:28:11 +0900

    modify function names in the api.js

--- src/api.js
+++ src/api.js
@@ -32,13 +32,13 @@ export const tvApi = {
   topRated: () => api.get("tv/top_rated"),
   popular: () => api.get("tv/popular"),
   airingToday: () => api.get("tv/airing_today"),
-  tvDetail: (id) =>
+  tvShowDetail: (id) =>
     api.get(`tv/${id}`, {
       params: {
         append_to_response: "videos",
       },
     }),
-  tvSearch: (term) =>
+  tvShowSearch: (term) =>
     api.get("search/tv", {
       params: {
         query: encodeURIComponent(term),
