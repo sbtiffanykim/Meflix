8c4a526516bc5c19fa4dff3b1e4583587419c998
Tiffnay Kim <sbtiffanykim@gmail.com>
2021-06-21 23:43:40 +0900

    configure the basic presenter structure

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,2 +1,16 @@
-// eslint-disable-next-line
-export default () => "Home";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const HomePresenter = ({ nowPlaying, upcoming, popular, error, loading }) =>
+  null;
+
+HomePresenter.propTypes = {
+  nowPlaying: PropTypes.array,
+  upcoming: PropTypes.array,
+  popular: PropTypes.array,
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.string,
+};
+
+export default HomePresenter;
--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,2 +1,23 @@
-// eslint-disable-next-line
-export default () => "Search";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const TVPresenter = ({
+  movieResults,
+  tvResults,
+  searchTerm,
+  loading,
+  handleSubmit,
+  error,
+}) => null;
+
+TVPresenter.propTypes = {
+  movieResults: PropTypes.array,
+  tvResults: PropTypes.array,
+  searchTerm: PropTypes.string,
+  loading: PropTypes.bool.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  error: PropTypes.string,
+};
+
+export default TVPresenter;
