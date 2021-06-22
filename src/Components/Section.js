9ae2526359ccb229b12b827f74f8a5c26b6cb764
Tiffnay Kim <sbtiffanykim@gmail.com>
2021-06-22 11:17:09 +0900

    add grid to the home presenter

--- src/Components/Section.js
+++ src/Components/Section.js
@@ -16,7 +16,9 @@ const Title = styled.span`
 
 const Grid = styled.div`
   margin-top: 20px;
-  margin-bottom: 50px;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 125px);
+  grid-gap: 25px;
 `;
 
 const Section = ({ title, children }) => (
--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -12,19 +12,25 @@ const HomePresenter = ({ nowPlaying, upcoming, popular, error, loading }) =>
     <Container>
       {nowPlaying && nowPlaying.length > 0 && (
         <Section title="Now Playing">
-          {nowPlaying.map((movie) => movie.title)}
+          {nowPlaying.map((movie) => (
+            <span key={movie.id}>{movie.title}</span>
+          ))}
         </Section>
       )}
 
       {upcoming && upcoming.length > 0 && (
         <Section title="Upcoming Movies">
-          {upcoming.map((movie) => movie.title)}
+          {upcoming.map((movie) => (
+            <span key={movie.id}>{movie.title}</span>
+          ))}
         </Section>
       )}
 
       {popular && popular.length > 0 && (
         <Section title="Popular Movies">
-          {popular.map((movie) => movie.title)}
+          {popular.map((movie) => (
+            <span key={movie.id}>{movie.title}</span>
+          ))}
         </Section>
       )}
     </Container>
